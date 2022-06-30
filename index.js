5987a79621b1024d542fdea0f75182f1399a05a5
Rajat-Saxena-11 <96068082+Rajat-Saxena-11@users.noreply.github.com>
2022-07-01 01:31:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+            var x = 0;
+
+            document.getElementById('output-area').innerHTML = x;
+
+            function button1() {
+                 document.getElementById('output-area').innerHTML = --x;
+            }
+
+            function button2() {
+                 document.getElementById('output-area').innerHTML = ++x;
+            }
+
+            var x = 0;
+
+            document.getElementById('output-cy').innerHTML = x;
+
+            function button3() {
+                 document.getElementById('output-cy').innerHTML = --x;
+            }
+
+            function button4() {
+                 document.getElementById('output-cy').innerHTML = ++x;
+            }
+
+            var x = 0;
+
+            document.getElementById('output-cz').innerHTML = x;
+
+            function button5() {
+                 document.getElementById('output-cz').innerHTML = --x;
+            }
+
+            function button6() {
+                 document.getElementById('output-cz').innerHTML = ++x;
+            }
+
+            var x = 0;
+
+            document.getElementById('output-cv').innerHTML = x;
+
+            function button7() {
+                 document.getElementById('output-cv').innerHTML = --x;
+            }
+
+            function button8() {
+                 document.getElementById('output-cv').innerHTML = ++x;
+            }
+
